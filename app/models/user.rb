fa5e4a445a5570fd93799c58f9b6b0616957ab49
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2021-01-12 02:41:32 +0900

    新規登録ログインエラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    validates :nickname,       presence: true, format: { with: /\A[a-z0-9ぁ-んァ-ン一-龥]+\z/ }, length: { maximum: 8 }
+    validates :nickname,       presence: true, format: { with: /\A[a-z0-9ぁ-んァ-ン一-龥]+\z/ }, length: { maximum: 6 }
     validates :password,       format: { with: /\A[a-z0-9]+\z/i }
 end

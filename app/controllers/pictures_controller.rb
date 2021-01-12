a1a6eb33d1921bce913be229dfe83496d1befb19
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2021-01-13 00:37:16 +0900

    newビュー実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :new]
 
   def index
     @pictures = Picture.all.order('created_at DESC')
@@ -8,7 +8,7 @@ class PicturesController < ApplicationController
   def new
     @picture = Picture.new
   end
-  
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end

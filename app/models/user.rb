f9a48e449ca1813bab0aa20cc0278a2f3184a951
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2021-01-12 17:51:00 +0900

    activestorage導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :pictures
     validates :nickname,       presence: true, format: { with: /\A[a-z0-9ぁ-んァ-ン一-龥]+\z/ }, length: { maximum: 6 }
     validates :password,       format: { with: /\A[a-z0-9]+\z/i }
 end

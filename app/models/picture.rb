3f38f7cd2bde5bf536d5bad5ffba4429b9191357
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2021-01-16 21:02:00 +0900

    ネスト（子）の削除実装

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :delete_all
   has_one_attached :image
   
   with_options presence: true do

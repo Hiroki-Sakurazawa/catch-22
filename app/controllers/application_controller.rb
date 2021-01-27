0a697c6a151222c101af1af55f63991a3fb6353a
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2021-01-27 09:13:49 +0900

    featuresビュー実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :take_features
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def take_features
+    @features = Picture.includes(:user).order('impressions_count DESC').take(5)
   end
 end
